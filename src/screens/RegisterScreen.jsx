1503b8c8684d4eb23a4824a98a870219c334de83
Maria Isadora <marisadora02@gmail.com>
2023-03-20 16:51:53 -0300

    SplashScreen e RegisterScreen

---
+++ src/screens/RegisterScreen.jsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { Paragraph, TextInput } from "react-native-paper";
+import { styles } from "../utils/styles"
+
+export default function RegisterScreen() {
+
+    return {
+        <View styles 
+    }
+
+}
