ea8950107678270d985906529cb68357f02bd2ae
Maria Isadora <marisadora02@gmail.com>
2023-03-22 15:41:58 -0300

    auth e .then

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -1,18 +1,45 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { View } from "react-native";
 import { Paragraph, TextInput } from "react-native-paper";
+import { auth } from "../config/firebase";
 import { styles } from "../utils/styles";
 export default function RegisterScreen() {
-  const [nome, setNome] = useState();
-  const [email, setEmail] = useState();
-  const [senha, setSenha] = useState();
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  function handleRegister(){
+    signInWithEmailAndPassword(auth, email, senha)
+    .then((userCredential) =>{
+        console.log("Usuário logado com sucesso!")
+    })
+    .catch((error) =>{
+        console.log("Erro ao logar usuário", error);
+    });
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.box}>
         <Paragraph>Realize o seu cadastro</Paragraph>
-        <TextInput label={"E-email"} placeholder="Digite seu e-mail" />
-        <TextInput placeholder="Digite sua senha" secureTextEntry={true} />
+        <TextInput 
+        label={"E-mail"} 
+        placeholder="Digite seu e-mail" 
+        value={email}
+        onChangeText={setEmail}
+        mode="outlined"
+        />
+
+        <TextInput 
+        label={"Senha"} 
+        placeholder="Digite sua senha" 
+        secureTextEntry={true}
+         value={senha} 
+         onChangeText={setSenha}
+         mode="outlined"
+         />
+
       </View>
     </View>
   );
